4b1c7934765506f1ca4af695be1bfc15d360febc
ELIZA <rysaeva96@mail.ru>
2022-05-20 19:54:04 +0500

    Решение 3 задачи

--- Task_3/Program3.cs
+++ Task_3/Program3.cs
@@ -7,3 +7,11 @@ Console.WriteLine("День -  " + num);
 if (num == 6 || num == 7) Console.WriteLine("Выходной день");
 else Console.WriteLine("Рабочий день");
 
+//Если вводить числа, соответствующие дням недели,
+//то программа будет такой
+
+Console.Write("Введите число, соответствующее дню недели = ");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 == 6 || num1 == 7) Console.WriteLine("Выходной день");
+else if (num1 > 0 && num1 < 6) Console.WriteLine("Рабочий день");
+else if (num1 > 7) Console.WriteLine("Такого дня не существует");
