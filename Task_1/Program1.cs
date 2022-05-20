c0a64ebae4b9395510dd8c652fbc433ee7401fd2
ELIZA <rysaeva96@mail.ru>
2022-05-20 19:20:23 +0500

    Решение 1 задачи

--- Task_1/Program1.cs
+++ Task_1/Program1.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int firstDigit = num / 10;
+int secondDigit = firstDigit % 10;
+Console.WriteLine(secondDigit);
